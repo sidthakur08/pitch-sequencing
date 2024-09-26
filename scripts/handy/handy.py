15ee0bc27ec1fa154d6d181591ce2cfd03706cf9
Kaelen <kaelenhaag@gmail.com>
2024-09-25 22:34:21 -0500

    docker image building and pushing

--- scripts/handy/handy.py
+++ scripts/handy/handy.py
@@ -3,7 +3,11 @@
 import argparse
 import os
 import shutil
+import subprocess
 
+from typing import Optional
+
+from pitch_sequencing.constants.gcloud import get_gcloud_account_username
 from pitch_sequencing.constants.project import get_project_base_dir
 
 
@@ -12,6 +16,8 @@ REGION_REPO="us-central1-docker.pkg.dev"
 PROJECT_ID="pitch-sequencing"
 REPO="pitch-sequencing-training"
 REGION="us-central1"
+DEFAULT_IMAGE_TAG_TEMPLATE="{username}-latest"
+FULL_DOCKER_IMAGE_URI_TEMPLATE="{region_repo}/{project_id}/{repo}/{image_name}:{tag}"
 
 
 LOCAL_BUILD_PATH=os.path.join(get_project_base_dir(), ".build")
@@ -29,9 +35,6 @@ def parse_args():
 
     return args
 
-
-def push_image(image_name: str):
-    pass
 
 def extract_target_directory(input_path: str) -> str:
     """
@@ -52,6 +55,9 @@ def extract_target_name(target_directory: str) -> str:
     return os.path.basename(target_directory)
 
 def validate_required_job_files_exist(build_directory: str):
+    """
+    Ensures all expected files are present in build directory. If not, raise exception.
+    """
     for required_file in REQUIRED_JOB_FILES:
         if not os.path.exists(os.path.join(build_directory, required_file)):
             raise Exception(f"Required file {required_file} missing from build directory")
@@ -73,11 +79,16 @@ def maybe_generate_empty_requirements_file(build_directory: str) -> bool:
 
 def maybe_generate_dockerfile(build_directory: str) -> bool:
     """
+    Checks if a Dockerfile is present in build directory, if not create a generic templated one.
+
+    Returns true if one was created.
     """
     dockerfile_path = os.path.join(build_directory, "Dockerfile")
     if os.path.exists(dockerfile_path):
         return False
-    
+
+    # We will set the docker build context to the project's root directory
+    # So we need our build directory's relative path to that when passing in. 
     rel_target_build_directory = os.path.relpath(build_directory, get_project_base_dir())
     
     default_dockerfile = f"""
@@ -135,6 +146,53 @@ def prepare_build_directory(target_directory_path: str) -> str:
 
     return target_build_directory
 
+def build_docker_image(target_name: str, build_directory: str, image_tag_override: Optional[str]) -> str:
+    """
+    Builds a Docker image where the Name of the image is built using target_name.
+    Leverages the build_directory's Dockerfile but sets the Docker Build context to the root project directory.
+    If image_tag_override is given, use that. Otherwise just use the {GCP username}-latest for tag.
+
+    Returns the full docker image URI.
+    """
+    image_tag = ""
+    if image_tag_override is not None:
+        image_tag = image_tag_override
+    else:
+        image_tag = DEFAULT_IMAGE_TAG_TEMPLATE.format(username=get_gcloud_account_username())
+    
+    docker_image_uri = FULL_DOCKER_IMAGE_URI_TEMPLATE.format(region_repo=REGION_REPO, project_id=PROJECT_ID, repo=REPO, image_name=target_name, tag=image_tag)
+    print(f"Building docker image {docker_image_uri}")
+    
+    try:
+        # Constructing the docker build command
+        command = [
+            "docker", "build", "-t", docker_image_uri, "-f", 
+            os.path.join(build_directory, "Dockerfile"), get_project_base_dir()
+        ]
+        
+        # Execute the docker build command
+        result = subprocess.run(command, check=True, text=True)
+        
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the subprocess
+        print("Failed to build Docker image:")
+        raise e
+
+    return docker_image_uri
+
+def push_docker_image(docker_image_uri: str):
+    try:
+        # Constructing the docker build command
+        command = [
+            "docker", "push", docker_image_uri
+        ]
+
+        # Execute the docker build command
+        result = subprocess.run(command, check=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print("Failed to push Docker Image")
+        raise e
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -147,3 +205,11 @@ if __name__ == "__main__":
     # Prepare temporary docker build directory
     build_directory = prepare_build_directory(target_directory_path)
     print(f"Build directory: {build_directory}")
+
+    docker_image_name = build_docker_image(target_name, build_directory, args.image_tag)
+    print(f"Successfully built {docker_image_name}")
+
+    push_docker_image(docker_image_name)
+    print("Succesfully pushed Docker Image to GCP")
+
+
