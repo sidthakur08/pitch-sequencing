88cf40521bf00c245ed5c5cb9d8ee18e077a9ff8
Kaelen Haag <kaelenhaag@gmail.com>
2024-10-07 12:50:01 -0500

    Tensorboard Logging during training (#10)

--- cmd/transformers/main.py
+++ cmd/transformers/main.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import train_test_split
 from collections import defaultdict
 
@@ -135,10 +136,13 @@ class PitchTransformer(nn.Module):
 
 
 
-def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, num_epochs: int, lr: float, device: torch.device, output_directory: str) -> nn.Module:
+def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, num_epochs: int, lr: float, device: torch.device, output_directory: str, logging_directory: str) -> nn.Module:
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    # SummaryWriter for Tensorboard logging metrics
+    summary_writer = SummaryWriter(log_dir=logging_directory)
 
     for epoch in range(num_epochs):
         model.train()
@@ -153,6 +157,9 @@ def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoad
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
+        
+        summary_writer.add_scalar("train/loss", train_loss, epoch)
+        summary_writer.add_scalar("train/avg_loss", train_loss/len(train_loader), epoch)
 
         model.eval()
         val_loss = 0
@@ -163,10 +170,17 @@ def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoad
                 output = model(input_seq)
                 loss = criterion(output, target)
                 val_loss += loss.item()
+            
+        summary_writer.add_scalar("val/loss", val_loss, epoch)
+        summary_writer.add_scalar("val/avg_loss", val_loss/len(val_loader), epoch)
 
+       
         elapsed_time = time.time() - epoch_start
-        
         print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss/len(train_loader):.4f}, Val Loss: {val_loss/len(val_loader):.4f}. Epoch Time {elapsed_time:.2f}sec")
+        summary_writer.flush()
+
+
+    summary_writer.close()
 
     saved_path = save_model_to_gcs(model, join_paths(output_directory, "final", "model.pth"))
     print(f"Saved final model to {saved_path}")
@@ -179,6 +193,7 @@ def parse_args():
 
     parser.add_argument("--input_path", type=str, required=True, help="Input Path to training data.")
     parser.add_argument("--output_directory", type=str, required=True, help="Directory path for all output artifacts from training job")
+    parser.add_argument("--logging_directory", type=str, required=True, help="Base directory where all logging files will be stored, including tensorboard")
     parser.add_argument("--num_epochs", type=int, default=10, help="Number of training epochs to run")
     parser.add_argument("--batch_size", type=int, default=32, help="Mini batch size for training")
     parser.add_argument("--learning_rate", type=float, default=0.001, help="Learning rate for optimizer")
@@ -218,6 +233,6 @@ if __name__ == "__main__":
     print("Starting Training")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using GPU: {torch.cuda.is_available()}")
-    _ = train_model(model, train_loader, test_loader, num_epochs=args.num_epochs, lr=args.learning_rate, device=device, output_directory=args.output_directory)
+    _ = train_model(model, train_loader, test_loader, num_epochs=args.num_epochs, lr=args.learning_rate, device=device, output_directory=args.output_directory, logging_directory=args.logging_directory)
 
     print(f"Done training model. Output can be found at {args.output_directory}")
