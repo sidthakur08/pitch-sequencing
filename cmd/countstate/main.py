try to do a correct form of attention masking and fix some other bugs

cmd/countstate/main.py
@@ -17,7 +17,7 @@ from pitch_sequencing.ml.models.last_pitch import LastPitchTransformerModel
 from pitch_sequencing.io.join import join_paths
 from pitch_sequencing.io.gcs import save_model_to_gcs
 
-USE_FOCAL_LOSS = False
+USE_FOCAL_LOSS = True
 
 
 def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, num_epochs: int, lr: float, device: torch.device, output_directory: str, logging_directory: str, output_len: int) -> nn.Module:
@@ -37,9 +37,9 @@ def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoad
         epoch_start = time.time()
         for batch in train_loader:
             # Don't use arsenal mask for now.
-            input_seq, target = [b.to(device) for b in batch]
+            input_seq, padding_mask, target = [b.to(device) for b in batch]
             optimizer.zero_grad()
-            output = model(input_seq)
+            output = model(input_seq, padding_mask)
             if USE_FOCAL_LOSS:
                 expanded_target = torch.zeros(len(target), output_len, device=device)
                 for i, t in enumerate(target):
@@ -57,8 +57,8 @@ def train_model(model: nn.Module, train_loader: DataLoader, val_loader: DataLoad
         with torch.no_grad():
             for batch in val_loader:
                 # Don't use arsenal mask for now.
-                input_seq, target = [b.to(device) for b in batch]
-                output = model(input_seq)
+                input_seq, padding_mask, target = [b.to(device) for b in batch]
+                output = model(input_seq, src_mask=padding_mask)
                 if USE_FOCAL_LOSS:
                     expanded_target = torch.zeros(len(target), output_len, device=device)
                     for i, t in enumerate(target):

src/pitch_sequencing/ml/data/count_state.py
@@ -26,7 +26,9 @@ class LastPitchSequenceWithCountDataset(Dataset):
         missing_last_pitch_raw_seq, last_pitch = extract_last_element_from_csv_seq(raw_pitch_seq)
         #missing_last_count_raw_seq, _ = extract_last_element_from_csv_seq(raw_count_seq)
 
-        input_seq = torch.tensor(self.seq_tokenizer.tokenize(missing_last_pitch_raw_seq, raw_count_seq), dtype=torch.long)
+        input_seq, padding_mask = self.seq_tokenizer.tokenize(missing_last_pitch_raw_seq, raw_count_seq)
+        input_seq = torch.tensor(input_seq, dtype=torch.long)
+        padding_mask = torch.tensor(padding_mask, dtype=torch.bool)
         target = torch.tensor(self.seq_tokenizer.get_id_for_pitch(last_pitch), dtype=torch.long)
 
-        return input_seq, target
+        return input_seq, padding_mask, target

src/pitch_sequencing/ml/models/last_pitch.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 class LastPitchTransformerModel(nn.Module):
     def __init__(self, vocab_size, d_model, nhead, num_layers, max_seq_len=1000, dropout=0.1):
         super(LastPitchTransformerModel, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        # Hardcode padding_idx to 0 for padding tokens in sequence. Add paramater in future 
+        self.embedding = nn.Embedding(vocab_size, d_model, padding_idx=0)
         self.pos_encoder = nn.Embedding(max_seq_len, d_model)  # Assuming max sequence length < 1000
         self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout),
+            nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout, batch_first=True),
             num_layers
         )
         self.fc = nn.Linear(d_model, vocab_size)