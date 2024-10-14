d118065f5e544bb32261f7f14769c70410aea661
Kaelen <kaelenhaag@gmail.com>
2024-10-14 16:25:38 -0500

    bug fix for position of output and introduce separate sequence model

--- src/pitch_sequencing/ml/models/last_pitch.py
+++ src/pitch_sequencing/ml/models/last_pitch.py
@@ -18,5 +18,27 @@ class LastPitchTransformerModel(nn.Module):
         pos = torch.arange(0, src.size(1), dtype=torch.long, device=src.device).unsqueeze(0)
         src = src + self.pos_encoder(pos)
         output = self.transformer(src, src_key_padding_mask=src_mask)
-        return self.fc(output[:, -1, :])  # Only use the last position for prediction
+        return self.fc(output[:, 0, :])  # Only use the first position for prediction
+    
+class SeparateEmbeddingLayersLastPitchTransformerModel(nn.Module):
+    def __init__(self, vocab_size, d_model, nhead, num_layers, max_seq_len=1000, dropout=0.1):
+        super(SeparateEmbeddingLayersLastPitchTransformerModel, self).__init__()
+        # Hardcode padding_idx to 0 for padding tokens in sequence. Add paramater in future 
+        self.pitch_seq_embedding = nn.Embedding(vocab_size, d_model, padding_idx=0)
+        self.count_seq_embedding = nn.Embedding(vocab_size, d_model, padding_idx=0)
+        self.pos_encoder = nn.Embedding(max_seq_len, d_model)  # Assuming max sequence length < 1000
+        self.transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout, batch_first=True),
+            num_layers
+        )
+        self.fc = nn.Linear(d_model, vocab_size)
+    
+    def forward(self, pitch_seq, count_seq, src_mask=None):
+        pitch_seq_embed = self.pitch_seq_embedding(pitch_seq)
+        count_seq_embed = self.count_seq_embedding(count_seq)
+        src = pitch_seq_embed+count_seq_embed
+        pos = torch.arange(0, src.size(1), dtype=torch.long, device=src.device).unsqueeze(0)
+        src = src + self.pos_encoder(pos)
+        output = self.transformer(src, src_key_padding_mask=src_mask)
+        return self.fc(output[:, 0, :])  # Only use the last position for prediction
     
